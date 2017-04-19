e1dc8802e491ad88ef423e428449990b886310c9
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2017-04-19 14:11:29 +0200

    Created an adaptive binarization

--- src/main/java/hr/fer/zemris/zavrad/Binarization.java
+++ src/main/java/hr/fer/zemris/zavrad/Binarization.java
@@ -8,6 +8,28 @@ package hr.fer.zemris.zavrad;
 public class Binarization implements ImageFilter {
     @Override
     public GrayScaleImage filter(GrayScaleImage ... image) {
-        return null;
+        if (image.length != 1){
+            throw new ImageFilterException("Binarization filter expects 1 argument!");
+        }
+        ImageFilter wiener = new WienerFilter(5, 5);
+        GrayScaleImage wienerResult = wiener.filter(image);
+
+        ImageFilter niblack = new NiblackThreshold(20, 20, -0.2);
+        GrayScaleImage niblackResult = niblack.filter(image);
+
+        ImageFilter interpolator = new BackgroundEstimation(3, 3);
+        GrayScaleImage background = interpolator.filter(wienerResult, niblackResult);
+
+        ImageFilter finalThreshold = new AdaptiveThreshold(0.8);
+        GrayScaleImage threshold = finalThreshold.filter(background, wienerResult, niblackResult);
+
+        int n = 5;
+        ImageFilter shrink = new ShrinkFilter(n, 0.8);
+        GrayScaleImage shrinked = shrink.filter(threshold);
+
+        ImageFilter swell1 = new Swell(n, 0.1);
+        GrayScaleImage swelled = swell1.filter(shrinked);
+
+        return swelled;
     }
 }
