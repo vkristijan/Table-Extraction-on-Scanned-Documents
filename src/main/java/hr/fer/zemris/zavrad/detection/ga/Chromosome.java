d8561848c072f1cabde49486cf54b659b6af43bb
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2017-05-08 00:19:41 +0200

    Created chromosome for GA  #2 Updated .gitignore

--- src/main/java/hr/fer/zemris/zavrad/detection/ga/Chromosome.java
+++ src/main/java/hr/fer/zemris/zavrad/detection/ga/Chromosome.java
@@ -1,8 +1,45 @@
 package hr.fer.zemris.zavrad.detection.ga;
 
+import hr.fer.zemris.zavrad.util.Rnd;
+
 /**
+ * A double array chromosome used in the genetic algorithm.
+ * Provides all the necessary methods to change the data.
+ *
  * @author Kristijan Vulinović
  * @version 1.0.0
  */
 public class Chromosome {
+    private static final double LOWER_BOUND = -1;
+    private static final double UPPER_BOUND = 1;
+
+    private double[] data;
+
+    public Chromosome(double[] data) {
+        this.data = data;
+    }
+
+    public Chromosome(int size){
+        data = new double[size];
+
+        for (int i = 0; i < size; ++i){
+            data[i] = Rnd.nextDouble(LOWER_BOUND, UPPER_BOUND);
+        }
+    }
+
+    public double[] getData(){
+        return data;
+    }
+
+    public void setData(double[] data){
+        this.data = data;
+    }
+
+    public int size(){
+        return data.length;
+    }
+
+    public void set(int index, double value){
+        data[index] = value;
+    }
 }
