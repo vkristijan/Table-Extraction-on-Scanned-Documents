0cf94776ee51448fc7cc0b2e3564514e72b8bd97
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2017-05-07 14:07:27 +0200

    Implemented weights for layers and FFANN

--- src/main/java/hr/fer/zemris/zavrad/detection/neural/FFANN.java
+++ src/main/java/hr/fer/zemris/zavrad/detection/neural/FFANN.java
@@ -5,4 +5,50 @@ package hr.fer.zemris.zavrad.detection.neural;
  * @version 1.0.0
  */
 public class FFANN {
+    private int[] layout;
+    private ActivationFunction[] activationFunctions;
+
+    private Layer[] layers;
+
+    public FFANN(int[] layout, ActivationFunction[] activationFunctions) {
+        this.layout = layout;
+        this.activationFunctions = activationFunctions;
+
+        for (int i = 0; i < layout.length; ++i){
+            Layer layer = new Layer(layout[i], activationFunctions[i]);
+            layers[i] = layer;
+        }
+    }
+
+    public int getWeightCount(){
+        int count = 0;
+
+        for (Layer layer : layers){
+            count += layer.getWeightCount();
+        }
+
+        return count;
+    }
+
+    public double[] getWeights(){
+        double[] weights = new double[getWeightCount()];
+
+        int index = 0;
+        for (Layer layer : layers){
+            double[] layerWeights = layer.getWeights();
+            System.arraycopy(layerWeights, 0, weights, index, layerWeights.length);
+            index += layerWeights.length;
+        }
+
+        return weights;
+    }
+
+    public void setWeights(double[] weights){
+        int index = 0;
+        for (Layer layer : layers){
+            double[] layerWeights = new double[layer.getWeightCount()];
+            System.arraycopy(weights, index, layerWeights, 0, layerWeights.length);
+            index += layerWeights.length;
+        }
+    }
 }
--- src/main/java/hr/fer/zemris/zavrad/detection/neural/Layer.java
+++ src/main/java/hr/fer/zemris/zavrad/detection/neural/Layer.java
@@ -1,14 +1,14 @@
 package hr.fer.zemris.zavrad.detection.neural;
 
-import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.RealMatrix;
-import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.*;
 
 /**
  * @author Kristijan Vulinović
  * @version 1.0.0
  */
 public class Layer {
+    private static final ArrayRealVector BIAS = new ArrayRealVector(new double[]{1});
+
     private Layer previous;
     private Layer next;
 
@@ -26,7 +26,21 @@ public class Layer {
         return values.getDimension();
     }
 
+    public void setNext(Layer layer){
+        this.next = layer;
+        layer.previous = this;
+    }
+
+    public void setPrevious(Layer layer){
+        this.previous = layer;
+        layer.next = this;
+    }
+
     public void setInput(RealVector input){
+        if (previous != null){
+            throw new NeuralException("Unable to set input for a layer other than the input layer!");
+        }
+
         if (input.getDimension() != values.getDimension()){
             throw new NeuralException("The size of the input vector is different than the size of the layer.");
         }
@@ -35,6 +49,48 @@ public class Layer {
     }
 
     public void setWeights(double[] weights){
+        int rows = previous.getSize() + 1;
+        int cols = getSize();
 
+        this.weights = MatrixUtils.createRealMatrix(rows, cols);
+
+        int index = 0;
+        for (int i = 0; i < rows; ++i){
+            double[] rowWeights = new double[cols];
+            System.arraycopy(weights, index, rowWeights, 0, cols);
+            index += cols;
+            this.weights.setRow(i, rowWeights);
+        }
+    }
+
+    public double[] getWeights(){
+        int rows = this.weights.getRowDimension();
+        int cols = this.weights.getColumnDimension();
+
+        double[] weights = new double[rows * cols];
+        int index = 0;
+        for (int i = 0; i < rows; ++i){
+            double[] rowWeights = this.weights.getRow(i);
+            System.arraycopy(rowWeights, 0, weights, index, cols);
+            index += cols;
+        }
+
+        return weights;
+    }
+
+    public RealVector getValues(){
+        return values;
+    }
+
+    public void calculateValues(){
+        RealVector input = new ArrayRealVector(BIAS, previous.getValues());
+        values = weights.preMultiply(input);
+    }
+
+    public int getWeightCount() {
+        int rows = previous.getSize() + 1;
+        int cols = getSize();
+
+        return rows * cols;
     }
 }
