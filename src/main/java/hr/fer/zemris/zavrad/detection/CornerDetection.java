e9a561f5a688a474bfb6c5780e535aff4e9232bf
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2017-05-07 20:43:52 +0200

    Implemented an FFANN to detect corners. This should close the issue #1

---
+++ src/main/java/hr/fer/zemris/zavrad/detection/CornerDetection.java
@@ -0,0 +1,96 @@
+package hr.fer.zemris.zavrad.detection;
+
+import hr.fer.zemris.zavrad.detection.neural.ActivationFunction;
+import hr.fer.zemris.zavrad.detection.neural.FFANN;
+import hr.fer.zemris.zavrad.detection.neural.NeuralException;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * @author Kristijan Vulinović
+ * @version 1.0.0
+ */
+public class CornerDetection {
+    private static final int INPUT_LAYER = 16;
+    private static final int HIDDEN_LAYER = 13;
+    private static final int OUTPUT_LAYER = 10;
+    private FFANN ffann;
+
+    public CornerDetection() {
+        int[] layout = new int[]{INPUT_LAYER, HIDDEN_LAYER, OUTPUT_LAYER};
+        ActivationFunction[] activationFunctions = new ActivationFunction[]{
+                ActivationFunction.LINEAR,
+                ActivationFunction.SIGMOID,
+                ActivationFunction.SIGMOID
+        };
+
+        ffann = new FFANN(layout, activationFunctions);
+    }
+
+    public void setWeights(double[] weights){
+        ffann.setWeights(weights);
+    }
+
+    public int getWeightCount(){
+        return ffann.getWeightCount();
+    }
+
+    public CornerValues detect(double[] input){
+        if (input.length != INPUT_LAYER){
+            throw new NeuralException("Wrong number of features in input vector.");
+        }
+
+        RealVector inputVector = new ArrayRealVector(input);
+        RealVector output = ffann.getOutput(inputVector);
+        double[] outputArray = output.toArray();
+
+        double maxValue = -1;
+        int index = 0;
+        for (int i = 0; i < outputArray.length; ++i){
+            if (outputArray[i] > maxValue){
+                maxValue = outputArray[i];
+                index = i;
+            }
+        }
+
+        return CornerValues.getCornerValue(index);
+    }
+
+    public void readWeightsFromFile(Path filePath){
+        List<String> lines = null;
+
+        try {
+            lines = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        double[] weights = new double[lines.size()];
+        for (int i = 0; i < weights.length; ++i){
+            weights[i] = Double.parseDouble(lines.get(i));
+        }
+
+        ffann.setWeights(weights);
+    }
+
+    public void writeWeightsToFile(Path filePath){
+        double[] weights = ffann.getWeights();
+
+        StringBuilder sb = new StringBuilder();
+        for (double weight : weights){
+            sb.append(weight);
+            sb.append(String.format("%n"));
+        }
+
+        try {
+            Files.write(filePath, sb.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/hr/fer/zemris/zavrad/detection/CornerValues.java
+++ src/main/java/hr/fer/zemris/zavrad/detection/CornerValues.java
@@ -1,18 +1,43 @@
 package hr.fer.zemris.zavrad.detection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Kristijan Vulinović
  * @version 1.0.0
  */
 public enum CornerValues {
-    UL_CORNER,
-    TOP,
-    UR_CORNER,
-    LEFT,
-    CENTER,
-    RIGHT,
-    DL_CORNER,
-    BOTTOM,
-    DR_CORNER,
-    NONE
+    UL_CORNER(0),
+    TOP(1),
+    UR_CORNER(2),
+    LEFT(3),
+    CENTER(4),
+    RIGHT(5),
+    DL_CORNER(6),
+    BOTTOM(7),
+    DR_CORNER(8),
+    NONE(9);
+
+    private int value;
+    private static Map<Integer, CornerValues> map;
+
+    CornerValues(int value) {
+        this.value = value;
+    }
+
+    static {
+        map = new HashMap<>();
+        for (CornerValues corner : CornerValues.values()){
+            map.put(corner.value, corner);
+        }
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public static CornerValues getCornerValue(int value){
+        return map.get(value);
+    }
 }
