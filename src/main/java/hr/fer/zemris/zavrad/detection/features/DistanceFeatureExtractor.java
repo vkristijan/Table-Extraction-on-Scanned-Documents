b4be115b9926ff7dfe3804301f00ea3ceb74576d
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2017-05-16 02:10:25 +0200

    Changed feature extraction

--- src/main/java/hr/fer/zemris/zavrad/detection/features/DistanceFeatureExtractor.java
+++ src/main/java/hr/fer/zemris/zavrad/detection/features/DistanceFeatureExtractor.java
@@ -8,7 +8,6 @@ import hr.fer.zemris.zavrad.util.img.GrayScaleImage;
  * @version 1.0.0
  */
 public class DistanceFeatureExtractor implements IFeatureExtractor {
-
     @Override
     public double[] getFeatures(GrayScaleImage img, int x, int y, int w, int h) {
         if (w != h){
@@ -17,14 +16,14 @@ public class DistanceFeatureExtractor implements IFeatureExtractor {
 
         CompositeFeature compositeFeature = new CompositeFeature();
         compositeFeature.addFeature(new HorizontalDistanceFeature(0.1));
-        compositeFeature.addFeature(new HorizontalDistanceFeature(0.3));
-        compositeFeature.addFeature(new HorizontalDistanceFeature(0.7));
         compositeFeature.addFeature(new HorizontalDistanceFeature(0.9));
 
         compositeFeature.addFeature(new VerticalDistanceFeature(0.1));
-        compositeFeature.addFeature(new VerticalDistanceFeature(0.3));
-        compositeFeature.addFeature(new VerticalDistanceFeature(0.7));
         compositeFeature.addFeature(new VerticalDistanceFeature(0.9));
+
+        compositeFeature.addFeature(new HorizontalLineFeature(0.2, 5));
+        compositeFeature.addFeature(new VerticalLineFeature(0.2, 5));
+        compositeFeature.addFeature(new DiagonalLineFeature(5));
 
         double[] features = new double[16];
         byte[][] data = img.getData();
